b11c8af60b25587e3e16a882cbdf44457e7684e1
Johannes (jotrorox) <mail@jotrorox.com>
2026-08-09 16:08:27 +0200

    Basic TCP Socket

---
+++ main/config.example.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define WIFI_SSID     "your-wifi-name"
+#define WIFI_PASSWORD "your-wifi-password"
+
+#define PORT                6667
+#define KEEPALIVE_IDLE      60
+#define KEEPALIVE_INTERVAL  10
+#define KEEPALIVE_COUNT     3
